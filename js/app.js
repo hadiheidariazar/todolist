9e10909041852bc27c0107bdd71f7c8068aa004f
hadi heidariazar <fakeemail@gmail.com>
2023-05-21 14:16:45 +0330

    set the edit Main todo section

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ const btnAddTodoElem = $.querySelector('.btn-add-todo')
 const btnClearAllTodosElem = $.querySelector('.btn-clear-all-todo')
 const containerTodosElem = $.querySelector('.container-todos-inner')
 const likeRemoveTodoElem = $.querySelector('.like-remove-todo')
+const likeEditTodoElem = $.querySelector('.like-edit-todo')
+const inputUpdateTextTodoElem = $.querySelector('.input-update-text-todo')
 const modalElem = $.querySelector('.modal')
 
 
 let inputAddTodoValue = null
 let todosArray = []
-let isUpdate = false
 
 inputAddTodoElem.addEventListener('keyup', e => {
 
@@ -18,31 +19,6 @@ inputAddTodoElem.addEventListener('keyup', e => {
 
     if (e.keyCode === 13) {
         if (inputAddTodoValue) {
-            if (isUpdate) {
-
-            } else {
-                let newTodo = {
-                    id: todosArray.length + 1,
-                    title: inputAddTodoValue,
-                    created_Date: createNotesDate()
-                }
-
-                todosArray.push(newTodo)
-
-                setItemTodoInfoInLocalStorage(todosArray)
-                generateTodoTemplateToDom(todosArray)
-                ClearInputValue()
-            }
-        }
-    }
-})
-
-btnAddTodoElem.addEventListener('click', () => {
-    inputAddTodoValue = inputAddTodoElem.value.trim()
-    if (inputAddTodoValue) {
-        if (isUpdate) {
-
-        } else {
             let newTodo = {
                 id: todosArray.length + 1,
                 title: inputAddTodoValue,
@@ -55,6 +31,23 @@ btnAddTodoElem.addEventListener('click', () => {
             generateTodoTemplateToDom(todosArray)
             ClearInputValue()
         }
+    }
+})
+
+btnAddTodoElem.addEventListener('click', () => {
+    inputAddTodoValue = inputAddTodoElem.value.trim()
+    if (inputAddTodoValue) {
+        let newTodo = {
+            id: todosArray.length + 1,
+            title: inputAddTodoValue,
+            created_Date: createNotesDate()
+        }
+
+        todosArray.push(newTodo)
+
+        setItemTodoInfoInLocalStorage(todosArray)
+        generateTodoTemplateToDom(todosArray)
+        ClearInputValue()
     }
 })
 
@@ -117,7 +110,7 @@ function generateTodoTemplateToDom(todosArray) {
                                                   </svg>
                                             </button>
                                             <ul class="dropdown-menu">
-                                              <li class="dropdown-item py-2 px-3"> 
+                                              <li class="dropdown-item py-2 px-3" onclick="editMainTodo(${note.id})" data-bs-toggle="modal" data-bs-target="#editModal"> 
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill me-1" viewBox="0 0 16 16">
                                                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                                                   </svg>
@@ -166,4 +159,26 @@ function removeMainTodo(todoIndex) {
         setItemTodoInfoInLocalStorage(allTodos)
     })
 
+}
+
+function editMainTodo(todoId) {
+    let allTodos = getItemTodoInfoInLocalStorage(todosArray)
+    todosArray = allTodos
+
+    todosArray.forEach(todo => {
+        if (todo.id === todoId) {
+            inputUpdateTextTodoElem.value = todo.title
+            let inputUpdateTextTodoValue = inputUpdateTextTodoElem.value.trim()
+
+            likeEditTodoElem.addEventListener('click', () => {
+                if (inputUpdateTextTodoValue) {
+                    todo.title = inputUpdateTextTodoElem.value.trim()
+                    todo.created_Date = createNotesDate()
+
+                    generateTodoTemplateToDom(todosArray)
+                    setItemTodoInfoInLocalStorage(todosArray)
+                }
+            })
+        }
+    })
 }
