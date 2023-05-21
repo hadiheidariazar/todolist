7bb15678d7602422ac95c3931d1bdf87d19e1ce9
hadi heidariazar <fakeemail@gmail.com>
2023-05-21 12:48:58 +0330

    bugfix and remove status section

--- js/app.js
+++ js/app.js
@@ -24,12 +24,11 @@ inputAddTodoElem.addEventListener('keyup', e => {
                 let newTodo = {
                     id: todosArray.length + 1,
                     title: inputAddTodoValue,
-                    status: false,
                     created_Date: createNotesDate()
                 }
-        
+
                 todosArray.push(newTodo)
-        
+
                 setItemTodoInfoInLocalStorage(todosArray)
                 generateTodoTemplateToDom(todosArray)
                 ClearInputValue()
@@ -47,12 +46,11 @@ btnAddTodoElem.addEventListener('click', () => {
             let newTodo = {
                 id: todosArray.length + 1,
                 title: inputAddTodoValue,
-                status: false,
                 created_Date: createNotesDate()
             }
-    
+
             todosArray.push(newTodo)
-    
+
             setItemTodoInfoInLocalStorage(todosArray)
             generateTodoTemplateToDom(todosArray)
             ClearInputValue()
@@ -61,7 +59,6 @@ btnAddTodoElem.addEventListener('click', () => {
 })
 
 btnClearAllTodosElem.addEventListener('click', () => {
-    // localStorage.removeItem('todos')
     localStorage.clear()
     location.reload()
     containerTodosElem.innerHTML = ''
@@ -131,12 +128,6 @@ function generateTodoTemplateToDom(todosArray) {
                                                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                                                   </svg>
                                                 حذف
-                                              </li>
-                                              <li class="dropdown-item py-2 px-3">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
-                                                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-                                                  </svg>
-                                                درحال انجام
                                               </li>
                                             </ul>
                                           </div>
